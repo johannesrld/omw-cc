94506623beefbfa607b2d263e396789202ab6e03
tauin <71744831+tauin@users.noreply.github.com>
2023-11-15 11:49:56 +0800

    chore(lua): formatting nonsense
    Signed-off-by: tauin <71744831+tauin@users.noreply.github.com>

--- scripts/plan9/controlled-consumption/message-handler.lua
+++ scripts/plan9/controlled-consumption/message-handler.lua
@@ -1,9 +1,9 @@
-local ui = require "openmw.ui"
+local ui = require 'openmw.ui'
 
 return {
-	eventHandler = {
-		__PLAN9_ControledConsumption_consumptionDenied = function ()
-			ui.showMessage("You have already consumed a potion within the last 5 seconds.")
-		end
-	}
+  eventHandler = {
+    __PLAN9_ControlledConsumption_consumptionDenied = function()
+      ui.showMessage 'You have already consumed a potion within the last 5 seconds.'
+    end,
+  },
 }
