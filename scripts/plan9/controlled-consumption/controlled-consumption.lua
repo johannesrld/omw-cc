38742a20daa25dcfcebbfad066a1ad19e9ed5e0b
Johannes Richard Levi Dickenson <jrld@machine.>
2023-11-16 22:58:37 +0800

    chore: update nonsense

--- scripts/plan9/controlled-consumption/controlled-consumption.lua
+++ scripts/plan9/controlled-consumption/controlled-consumption.lua
@@ -8,22 +8,45 @@ local ItemUsage = require 'openmw.interfaces'.ItemUsage
 local async = require 'openmw.async'
 
 ---@type boolean
+local async = require "openmw.async"
+local types = require "openmw.types"
+local I = require "openmw.interfaces"
 local disableConsumption = false
+local enableConsumption = async:registerTimerCallback("enableConsumption", function() disableConsumption = false end)
 
-local enableConsumption =
-  async:registerTimerCallback('enableConsumption',
-    function()
-      disableConsumption = false
-    end)
+---@type table<string, {max: number, current: number}>
+local players = {}
 
-ItemUsage.addHandlerForType(types.Potion,
-  function(_, actor)
-    if actor.type ~= types.Player then return true end
-    if disableConsumption == true then
-      actor:sendEvent '__PLAN9_ControlledConsumption_consumptionDenied'
-      return false
-    end
-    async:newSimulationTimer(5, enableConsumption)
-    disableConsumption = true
-    return true
-  end)
+
+---@param actor GameObject
+local function ConsumptionCheck(_, actor)
+	if actor.type ~= types.Player then return true end
+	if disableConsumption == true then
+		actor:sendEvent("__PLAN9_ControlledConsumption_consumptionDenied")
+		return false
+	end
+	disableConsumption = true
+	async:newSimulationTimer(5, enableConsumption)
+	return true
+end
+
+I.ItemUsage.addHandlerForType(types.Potion, ConsumptionCheck)
+
+return {
+	eventHandlers = {
+		---@param data {plr_id: string, new_max: number}
+		__PLAN9_ControlledConsumption_updatePlayerData_newMax = function(data)
+			if players[data.plr_id] == nil then
+				players[data.plr_id] = { max = 1, current = 0 }
+			end
+			players[data.plr_id].max = math.max(data.new_max, 1)
+		end,
+		---@param data {plr_id: string}
+		__PLAN9_ControlledConsumption_updatePlayerData_potionExpired = function(data)
+			if players[data.plr_id] == nil then
+				players[data.plr_id] = { max = 1, current = 0 }
+			end
+			players[data.plr_id].current = math.max((players[data.plr_id].current - 1), 0)
+		end
+	}
+}
--- scripts/plan9/controlled-consumption/message-handler.lua
+++ scripts/plan9/controlled-consumption/message-handler.lua
@@ -1,9 +1,27 @@
-local ui = require 'openmw.ui'
+local ui = require "openmw.ui"
+local NPC = require "openmw.types".NPC
+local self = require "openmw.self"
+local core = require "openmw.core"
 
+local playerEndurance = NPC.baseType.stats.attributes.endurance(self)
+local playerAlchemy = NPC.stats.skills.alchemy(self)
+local currentMaximum = 1
+local function calculateMaximumPotions()
+	local maximumActivePotions = math.max(math.floor((playerEndurance.base + playerAlchemy.base) / 25), 1)
+	if maximumActivePotions ~= currentMaximum then
+		core.sendGlobalEvent("__PLAN9_ControlledConsumption_updatePlayerData_newMax",
+			{ plr_id = self.object.id, new_max = maximumActivePotions })
+	end
+end
+
+local a = NPC.baseType.activeEffects(self)
 return {
-  eventHandler = {
-    __PLAN9_ControlledConsumption_consumptionDenied = function()
-      ui.showMessage 'You have already consumed a potion within the last 5 seconds.'
-    end,
-  },
+	engineHandlers = {
+		onUpdate = calculateMaximumPotions
+	},
+	eventHandlers = {
+		__PLAN9_ControlledConsumption_consumptionDenied = function()
+			ui.showMessage("You have already consumed a potion within the last 5 seconds.")
+		end
+	}
 }
